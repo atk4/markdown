afdf8fcb52c9beee8e73f8c6aeba71b10dfdfee0
Vadym <vadym@agile55.com>
2015-03-12 15:25:08 +0200

    composer ready

---
+++ lib/atk4/markdown/Config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vadym
+ * Date: 12/03/15
+ * Time: 20:22
+ */
+
+namespace atk4\markdown;
+
+class Config {
+
+    private static $instance;
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+
+    protected function __construct() {}
+
+}
