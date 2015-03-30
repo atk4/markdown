fea15ec1432c25e83bfcf5c1dd54bf649f939180
Vadym <vadym@agile55.com>
2015-03-30 12:37:54 +0300

    fix

--- public/js/gme/button-bar.js
+++ public/js/gme/button-bar.js
@@ -229,7 +229,7 @@ function markUp(btn, txt) {
 			txt = prompt('Insert video ID', '')
 		}
 		str = (txt != null && txt != '') ?
-			'\n!<iframe title="YouTube video player" width="480" height="390" ' +
+			'\n<iframe title="YouTube video player" width="480" height="390" ' +
 			'src="http://www.youtube.com/embed/' + txt + '" ' +
 			'frameborder="0" allowfullscreen></iframe>\n\n' : txt;
 	}
